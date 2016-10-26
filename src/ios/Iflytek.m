a3fe47a7123996f216b5d4742a38d6e2a294b8c8
alexfhc <alexfang@heytz.com>
2016-10-26 19:04:01 +0800

    iOS commit

--- src/ios/Iflytek.m
+++ src/ios/Iflytek.m
@@ -1,113 +1,104 @@
-#import <Foundation/Foundation.h>
-#import <Cordova/CDV.h>
-#import "HiJoine.h"
+/********* Iflytek.m Cordova Plugin Implementation *******/
 
-@interface lsdwrapper : CDVPlugin <HiJoineDelegate> {
-    // Member variables go here.
-    HiJoine * joine;
-    //EASYLINK *easylink_config;
-    NSMutableDictionary *deviceIPConfig;
-    NSString *loginID;
+#import <Cordova/CDV.h>
+#import "IATViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#import "Definition.h"
+#import "ISRDataHelper.h"
+#import "IATConfigViewController.h"
+#import "IATConfig.h"
+
+@interface Iflytek : CDVPlugin<IFlySpeechRecognizerDelegate> {
+    IFlySpeechRecognizer *iFlySpeechRecognizer;
+    NSString * resultFromJson;
     CDVInvokedUrlCommand * commandHolder;
-    NSString *deviceIp ;
-    NSString *userToken ;
-    int acitvateTimeout;
-    NSString* activatePort;
-    NSString* bssid;
-    //
-    NSString* deviceLoginId;
-    NSString* devicePass;
-//    NSTimer * _timer;
 }
-- (void)setDeviceWifi:(CDVInvokedUrlCommand*)command;
-- (void)dealloc:(CDVInvokedUrlCommand*)command;
+
+- (void)VoiceRecognitionStart:(CDVInvokedUrlCommand*)command;
 @end
 
-//static long  _times = 0;
-
-@implementation lsdwrapper
+@implementation Iflytek
 -(void)pluginInitialize{
-
+    NSString *initString = [[NSString alloc] initWithFormat:@"appid=%@",@"54dd7faf"];
+    [IFlySpeechUtility createUtility:initString];
+    
 }
-
-- (void)setDeviceWifi:(CDVInvokedUrlCommand*)command
+- (void)VoiceRecognitionStart:(CDVInvokedUrlCommand*)command
 {
-
-    NSString* wifiSSID = [command.arguments objectAtIndex:0];
-    NSString* wifiKey = [command.arguments objectAtIndex:1];
-    loginID = [command.arguments objectAtIndex:2];
-    deviceLoginId = [command.arguments objectAtIndex:6];
-    devicePass = [command.arguments objectAtIndex:7];
-    int easylinkVersion;
-    activatePort = [command.arguments objectAtIndex:5];
+    //todo 需要添加权限的判断
+    iFlySpeechRecognizer = [IFlySpeechRecognizer sharedInstance];
+    [iFlySpeechRecognizer setParameter: @"iat" forKey: [IFlySpeechConstant IFLY_DOMAIN]];
+     iFlySpeechRecognizer.delegate = self;
+    if (iFlySpeechRecognizer != nil) {
+        IATConfig *instance = [IATConfig sharedInstance];
+        
+        //设置最长录音时间
+        [iFlySpeechRecognizer setParameter:instance.speechTimeout forKey:[IFlySpeechConstant SPEECH_TIMEOUT]];
+        //设置后端点
+        [iFlySpeechRecognizer setParameter:instance.vadEos forKey:[IFlySpeechConstant VAD_EOS]];
+        //设置前端点
+        [iFlySpeechRecognizer setParameter:instance.vadBos forKey:[IFlySpeechConstant VAD_BOS]];
+        //网络等待时间
+        [iFlySpeechRecognizer setParameter:@"20000" forKey:[IFlySpeechConstant NET_TIMEOUT]];
+        
+        //设置采样率，推荐使用16K
+        [iFlySpeechRecognizer setParameter:instance.sampleRate forKey:[IFlySpeechConstant SAMPLE_RATE]];
+        
+        if ([instance.language isEqualToString:[IATConfig chinese]]) {
+            //设置语言
+            [iFlySpeechRecognizer setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+            //设置方言
+            [iFlySpeechRecognizer setParameter:instance.accent forKey:[IFlySpeechConstant ACCENT]];
+        }else if ([instance.language isEqualToString:[IATConfig english]]) {
+            [iFlySpeechRecognizer setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+        }
+        //设置是否返回标点符号
+        [iFlySpeechRecognizer setParameter:instance.dot forKey:[IFlySpeechConstant ASR_PTT]];
+        
+    }
+    [iFlySpeechRecognizer setParameter:@"asrview.pcm" forKey:[IFlySpeechConstant ASR_AUDIO_PATH]];
+    [iFlySpeechRecognizer startListening];
     commandHolder = command;
-    if (joine !=nil) {
-        [joine cancelBoardData];
-    }
-    if ([command.arguments objectAtIndex:3] == nil || [command.arguments objectAtIndex:4] == nil) {
-        NSLog(@"Error: arguments easylink_version & timeout");
-        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        return;
-    }else {
-        easylinkVersion = [[command.arguments objectAtIndex:3] intValue];
-        acitvateTimeout = [[command.arguments objectAtIndex:4] intValue];
-    }
-
-    if (wifiSSID == nil || wifiSSID.length == 0 || wifiKey == nil || wifiKey.length == 0 || loginID == nil || loginID.length == 0 || activatePort==nil || activatePort.length == 0 || deviceLoginId == nil || deviceLoginId.length == 0
-        || devicePass == nil || devicePass.length==0) {
-        NSLog(@"Error: arguments");
-        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        return;
-    }
-//    _times = 0;
-//    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(counteTime) userInfo:nil repeats:YES];
-//    [_timer fire];
-    joine = [[HiJoine alloc] init];
-    joine.delegate = self;
-        [joine setBoardDataWithPassword:wifiKey withBackBlock:^(NSInteger result, NSString *message) {
-            if (result == 1) {
-                @try{
-                    NSDictionary *ret = [NSDictionary dictionaryWithObjectsAndKeys:
-                                         message, @"mac",
-                                         nil];
-                    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[ret objectForKey:@"mac"]];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:commandHolder.callbackId];
-                }
-                @catch (NSException *e){
-                    NSLog(@"error - save configuration..." );
-                    CDVPluginResult *pluginResult = nil;
-                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:commandHolder.callbackId];
-                }
-                // NSString *successStr = [NSString stringWithFormat:@"MAC地址 %@ 连接成功，耗时 %ld 秒", message];
-            }else{
-                CDVPluginResult *pluginResult = nil;
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:commandHolder.callbackId];
-            }
-        }];
 
 }
-- (void)dealloc:(CDVInvokedUrlCommand*)command
+- (void) onResults:(NSArray *) results isLast:(BOOL)isLast
 {
-    NSLog(@"//====dealloc...====");
-    if (joine !=nil) {
-        [joine cancelBoardData];
+    NSMutableString *resultString = [[NSMutableString alloc] init];
+    NSDictionary *dic = results[0];
+    for (NSString *key in dic) {
+        [resultString appendFormat:@"%@",key];
+    }
+    NSString *_result =[NSString stringWithFormat:@"%@",resultString];
+    NSLog(@"_result=%@",_result);
+    resultFromJson =[NSString stringWithFormat:@"%@%@",resultFromJson,[ISRDataHelper stringFromJson:resultString]];
+    NSLog(@"_result=%@",resultFromJson);
+    if (resultFromJson != nil && isLast==true) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:resultFromJson];
+        resultFromJson＝@“”;
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:commandHolder.callbackId];
     }
 
 }
-
-//- (void)endSending
-//{
-//    [_timer invalidate];
-//    _timer = nil;
-//}
-//
-//- (void)counteTime
-//{
-//    _times ++;
-//}
-
+- (void)onError: (IFlySpeechError *) error
+{
+    if (error.errorCode == 0 ) {
+        if (resultFromJson.length == 0) {
+            NSLog(@"_result=%@",@"no result");
+        }else {
+            NSLog(@"_result=%@",resultFromJson);
+        }
+    }else{
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:commandHolder.callbackId];
+    }
+}
+- (void) onEndOfSpeech
+{
+}
+- (void) onBeginOfSpeech
+{
+}
+- (void) onVolumeChanged: (int)volume
+{
+}
 @end
